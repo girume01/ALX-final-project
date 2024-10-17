801e33843a2853281495fa67443848007e37dd18
girum <girumassefa6@gmail.com>
2024-10-17 20:35:36 +0300

    ALX-final_portfolio

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.zw6hky5.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.connect(url, {
